b9f072e8fe86b68803ec84bd25bf5a691f82364e
Antoni <antoni.protoni@gmail.com>
2022-02-17 14:26:48 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+        </Switch>
+        <Switch>
+          <Route exact path="/contacts">
+            <h1 className="">
+              agregar nuevo contacto
+            </h1>
+            <input type="text"
+              className=
+              "rounded-md block border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route exact path="/edit">
+            <h1 className="">
+              Editar contacto
+            </h1>
+            <input type="text"
+              className=
+              "rounded-md block border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+
+          </Route>
+          <Route exact path="/">
+            <h1>Lista de contactos</h1>
+          </Route>
+          <Route render={() => <h1>Not found</h1>} />
+        </Switch>
+
+      </BrowserRouter>
+    </>
   );
 }
 
