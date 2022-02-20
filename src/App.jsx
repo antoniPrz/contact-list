f1446c82f120f3f0c295393d8c99aaf77af26713
Antoni <antoni.protoni@gmail.com>
2022-02-19 23:35:45 -0300

    fix router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,27 +11,39 @@ import { AddNewContact } from "./views/AddNewContact";
 import { EditContact } from "./views/EditContact";
 import { Home } from "./views/Home";
 
+
+
+
+
+
 function App() {
+
+
+
+  useEffect(() => {
+
+    fetch("https://assets.breatheco.de/apis/fake/contact/agenda/jarrod")
+      .then((batata) => (batata.json()))
+      .then((data) => { console.log(data) })
+
+  }, []);
+
+
   return (
     <>
       <BrowserRouter>
         <Switch>
-        </Switch>
-        <Switch>
           <Route exact path="/AddNew">
             <AddNewContact />
           </Route>
-        </Switch>
-        <Switch>
           <Route exact path="/edit">
             <EditContact />
           </Route>
           <Route exact path="/">
             <Home />
           </Route>
-          {/* <Route render={() => <h1>Not found</h1>} /> */}
+          <Route render={() => <h1>Not found</h1>} />
         </Switch>
-
       </BrowserRouter>
     </>
   );
